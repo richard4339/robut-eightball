26c3849e23e059b20f841e8e0163c73b290cb894
Richard Lynskey <richard@mozor.net>
2013-12-21 14:10:17 -0600

    Added tests

--- test/test_robut-eightball.rb
+++ test/test_robut-eightball.rb
@@ -1,7 +1,31 @@
-require 'helper'
+require 'test_helper'
+require 'robut'
+require 'robut-eightball'
+require 'mocha/setup'
 
-class TestRobutEightball < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class Robut::Plugin::EightBallTest < Test::Unit::TestCase
+
+  def setup
+    @connection = Robut::ConnectionMock.new
+    @presence = Robut::PresenceMock.new(@connection)
+    @plugin = Robut::Plugin::EightBall.new(@presence)
+  end
+
+  def test_handle_8ball_no_robut
+    @plugin.stubs(:random).returns(1)
+    @plugin.handle(Time.now, "John", "8ball something")
+    assert_equal( [], @plugin.reply_to.replies )
+  end
+
+  def test_handle_8ball_no_question
+    @plugin.stubs(:random).returns(1)
+    @plugin.handle(Time.now, "John", "@robut 8ball")
+    assert_equal( [], @plugin.reply_to.replies )
+  end
+
+  def test_handle_8ball_normal
+    @plugin.stubs(:random).returns(1)
+    @plugin.handle(Time.now, "John", "@robut 8ball something")
+    assert_equal( ["It is decidedly so"], @plugin.reply_to.replies )
   end
 end
